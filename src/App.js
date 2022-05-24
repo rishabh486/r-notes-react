38a6c08ef61ab3c7882cefadc4b531ed122e2a92
rishabh486 <76812565+rishabh486@users.noreply.github.com>
2022-05-24 23:18:18 +0530

    Auth (#1)
    * auth-context-added
    
    * auth-context-added
    
    * signin-signup-pages-added
    
    * environment-variable-added
    
    * routes-added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import "./App.css";
-
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./Components/Navbar/navbar";
+import SignInPage from "./Pages/SignInPage/SignInPage";
+import SignUpPage from "./Pages/SignUpPage/SignUpPage";
 function App() {
   return (
     <div className="App">
-      <div>hello</div>
+      <Navbar />
+      <Routes>
+        <Route path="/signin-page" element={<SignInPage />} />
+        <Route path="/signup-page" element={<SignUpPage />} />
+      </Routes>
     </div>
   );
 }
