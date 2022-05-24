e47bb918848b36bfdce66654856593f5acbab528
rishabh486 <76812565+rishabh486@users.noreply.github.com>
2022-05-24 23:08:40 +0530

    signin-signup-pages-added

---
+++ src/Pages/SignInPage/SignInPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Signin from "../../Components/Signin/signin";
+
+function SignInPage() {
+  return (
+    <div>
+      <Signin />
+    </div>
+  );
+}
+
+export default SignInPage;
